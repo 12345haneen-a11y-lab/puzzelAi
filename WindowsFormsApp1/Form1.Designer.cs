09ced2e0a44c402b8546f8b866745aff520572be
hanin <114311397+12345haneen-a11y-lab@users.noreply.github.com>
2024-03-06 19:09:54 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button9 = new WindowsFormsApp1.puzzel();
+            this.button8 = new WindowsFormsApp1.puzzel();
+            this.button7 = new WindowsFormsApp1.puzzel();
+            this.button6 = new WindowsFormsApp1.puzzel();
+            this.button5 = new WindowsFormsApp1.puzzel();
+            this.button4 = new WindowsFormsApp1.puzzel();
+            this.button3 = new WindowsFormsApp1.puzzel();
+            this.button2 = new WindowsFormsApp1.puzzel();
+            this.button1 = new WindowsFormsApp1.puzzel();
+            this.SuspendLayout();
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(468, 326);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(125, 63);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(270, 326);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 63);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(50, 326);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 63);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(468, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 63);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(270, 184);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 63);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(50, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 63);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(468, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 63);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private puzzel button1;
+        private puzzel button2;
+        private puzzel button3;
+        private puzzel button4;
+        private puzzel button5;
+        private puzzel button6;
+        private puzzel button7;
+        private puzzel button8;
+        private puzzel button9;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<puzzel> puzzels = new List<puzzel>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+            shuffle();
+        }
+        void init()
+        {
+            puzzels.Add(button1);
+            puzzels.Add(button2);
+            puzzels.Add(button3);
+            puzzels.Add(button4);
+            puzzels.Add(button5);
+            puzzels.Add(button6);
+            puzzels.Add(button7);
+            puzzels.Add(button8);
+            puzzels.Add(button9);
+            place(puzzels[0], null, button2, null, button4);
+            place(puzzels[1], button1, button3, null, button5);
+            place(puzzels[2], button2, null, null, button6);
+            place(puzzels[3], null, button5, button1, button7);
+            place(puzzels[4], button4, button6, button2, button8);
+            place(puzzels[5], button5, null, button3, button9);
+            place(puzzels[6], null, button8, button4, null);
+            place(puzzels[7], button7, button9, button5, null);
+            place(puzzels[8], button8, null, button6, null);
+
+
+        }
+        void place(puzzel x, puzzel left, puzzel right, puzzel up, puzzel down)
+        {
+            x.left = left;
+            x.right = right;
+            x.up = up;
+            x.down = down;
+
+        }
+        void shuffle()
+        {
+           List<int> nums = new List<int> { 1,2,3,4,5,6,7,8,9 };
+            Random r = new Random();
+            nums = nums.OrderBy(x=>r.Next()).ToList();
+            for (int i = 0; i < puzzels.Count; i++)
+            {
+                if (nums[i]==9)
+                {
+                    puzzels[i].Text = "";
+                }
+                else
+                {
+                    puzzels[i].Text = nums[i].ToString();
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (((puzzel)sender).left!=null && ((puzzel)sender).left.Text=="")
+            {
+                swap(((puzzel)sender).left, (puzzel)sender);
+            }
+            else if (((puzzel)sender).right != null && ((puzzel)sender).right.Text == "")
+            {
+                swap(((puzzel)sender).right, (puzzel)sender);
+
+            }
+            else if (((puzzel)sender).up != null && ((puzzel)sender).up.Text == "")
+            {
+                swap(((puzzel)sender).up, (puzzel)sender);
+
+            }
+            else if (((puzzel)sender).down != null && ((puzzel)sender).down.Text == "")
+            {
+                swap(((puzzel)sender).down, (puzzel)sender);
+
+            }
+            if (goal())
+            {
+                MessageBox.Show("you win");
+            }
+        }
+        void swap(puzzel a, puzzel b)
+        {
+            string temp = a.Text;
+            a.Text = b.Text;
+            b.Text = temp;
+
+        }
+        bool goal()
+        {
+            for (int i = 0; i < puzzels.Count; i++)
+            {
+                if (!(i<8 &&  puzzels[i].Text ==(i+1).ToString() || i == 8 && puzzels[i].Text ==""))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
